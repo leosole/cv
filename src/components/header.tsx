d83391f74eabcde6f99f987054b4c4c72828ae7c
leosole <leosole@gmail.com>
2025-09-30 14:15:29 +0000

    fix config and add cv json

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+
+export function Header() {
+  return (
+    <header className='container mx-auto'>
+      <h1 className="text-3xl font-bold mt-4">
+        Leonardo Solé Rodrigues
+      </h1>
+    </header>
+  )
+}
