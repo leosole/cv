ecc159c76cd27b45bbdbfd8aad3072b892b5a3d2
leosole <leosole@gmail.com>
2025-10-16 17:11:10 +0000

    add tooltip to flags

--- src/components/flags.tsx
+++ src/components/flags.tsx
@@ -1,6 +1,9 @@
 import { GB, DE, ES, BR, type FlagComponent } from 'country-flag-icons/react/3x2'
+import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from './ui/tooltip'
+import { useState } from 'react'
 
 export default function Flag({percent, country, title}: {percent: number, country: string, title?: string}) {
+    const [open, setOpen] = useState(false)
     let Component: FlagComponent
     switch (country) {
         case "GB":
@@ -16,11 +19,17 @@ export default function Flag({percent, country, title}: {percent: number, countr
             Component = BR
     }
     return (
-        <span className='flex' title={title}>
-            <span className='relative'>
-                <span className='absolute bg-white mix-blend-hue h-4 z-1 right-0' style={{width: (100 - percent)*24/100}}/>
-                <Component className='w-6 relative'/>
-            </span>
-        </span>
+        <Tooltip onOpenChange={() => setOpen(p => !p)} open={open}>
+            <TooltipTrigger onClick={() => setOpen(p => !p)}>
+                <span className='relative'>
+                    <span className='absolute bg-white mix-blend-hue h-4 z-1 right-0' style={{width: (100 - percent)*24/100}}/>
+                    <Component className='w-6 relative'/>
+                </span>
+            </TooltipTrigger>
+            <TooltipContent arrowPadding={12}>
+                {title}
+                <TooltipArrow />
+            </TooltipContent>
+        </Tooltip>
     )
 }
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react"
 import ThemeProvider from "./theme-provider"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<StrictMode>
-			<ThemeProvider>{children}</ThemeProvider>
+			<TooltipProvider delayDuration={100} skipDelayDuration={0}>
+				<ThemeProvider>{children}</ThemeProvider>
+			</TooltipProvider>
 		</StrictMode>
 	)
 }
